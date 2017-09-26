480ff98f22dab4c553ffbccca8db45a33b8ab375
StephanSo <s.sonnois@gmail.com>
2017-09-26 15:24:53 +0200

    Ajout d'un formulaire pour add jouet

--- server.js
+++ server.js
@@ -38,6 +38,25 @@ app.get('/jouets',
     lesJouets.forEach(
         (unJouet)=>{responseText += `Jouet : ${unJouet.libelle} <br />`;}
         );
+        responseText += '<hr>';
+        responseText += 'Ajout d\'un nouveau jouet </br>';
+        responseText += '<form action="/jouets" method="post">';
+        responseText += 'Libelle :<br>';
+        responseText += '<input type="text" name="libelle" value=""><br>';
+        responseText += 'Categorie :<br>';
+        responseText += '<select>';
+        responseText += `<option value="cat1"> ${categorie1.libelle}</option>`;
+        responseText += `<option value="cat2"> ${categorie2.libelle}</option>`;
+        responseText += `<option value="cat3"> ${categorie3.libelle}</option>`;
+        responseText +='</select><br>';
+        responseText += 'Tranche d\'âge :<br>';
+        responseText += '<select>';
+        responseText += `<option value="t02"> ${tranche02.toString()}</option>`;
+        responseText += `<option value="t25"> ${tranche25.toString()}</option>`;
+        responseText += `<option value="t48"> ${tranche48.toString()}</option>`;
+        responseText +='</select><br>';
+        responseText += '<input type="submit" value="Envoyer">';
+        responseText += '</form>';
     res.send(responseText);
     });
 
@@ -53,6 +72,10 @@ app.get('/jouets/:id',
         res.status(404).send('Sorry ! Toy doesn\'t exist');
     }
     });
+
+
+
+
 
 //Demarrage du serveur
 
