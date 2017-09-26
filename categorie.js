47f01f0b0a9fcba3df48f103560301f17629a67a
StephanSo <s.sonnois@gmail.com>
2017-09-26 09:58:23 +0200

    Creation de la classe catégorie

---
+++ categorie.js
@@ -0,0 +1,11 @@
+class Categorie{
+
+    constructor(unLibelle){
+        this._libelle = unLibelle;
+    }
+
+    get libelle(){
+        return this._libelle;
+    }
+}
+module.exports = Categorie;
